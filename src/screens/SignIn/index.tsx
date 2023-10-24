3daf601720910e0cc71b4827cb55ee4391d8077a
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-24 20:20:46 -0300

    feat: add KeyboardAvoidingView page sign in

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -6,56 +6,48 @@ import {
   Heading,
   Image,
   KeyboardAvoidingView,
+  ScrollView,
   Text,
   VStack,
 } from "native-base";
 import React from "react";
-import {
-  Keyboard,
-  Platform,
-  ScrollView,
-  TouchableWithoutFeedback,
-} from "react-native";
+import { Keyboard, Platform, TouchableWithoutFeedback } from "react-native";
 
 export function SignIn() {
   return (
     <KeyboardAvoidingView
+      h={{
+        base: "full",
+        lg: "auto",
+      }}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{ flex: 1 }}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <ScrollView contentContainerStyle={{ flex: 1 }}>
-          <VStack flex={1} bg="gray.700">
-            <Image
-              source={BackGroundImg}
-              alt="people training"
-              resizeMode="contain"
-              position="absolute"
+        <ScrollView flex={1} bg="gray.700">
+          <Image
+            source={BackGroundImg}
+            alt="people training"
+            resizeMode="contain"
+            position="absolute"
+          />
+          <Center my={24}>
+            <LogoSVG />
+            <Text fontSize="sm" fontWeight="bold" color="gray.100">
+              Treine sua mente e seu corpo
+            </Text>
+          </Center>
+          <Center my={24}>
+            <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
+              Acesse sua conta
+            </Heading>
+          </Center>
+          <VStack flex={1} space={4} mb={6} px={10}>
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
             />
-            <Center my={24}>
-              <LogoSVG />
-              <Text fontSize="sm" fontWeight="bold" color="gray.100">
-                Treine sua mente e seu corpo
-              </Text>
-            </Center>
-            <Center my={24}>
-              <Heading
-                color="gray.100"
-                fontSize="xl"
-                fontFamily="heading"
-                mb={6}
-              >
-                Acesse sua conta
-              </Heading>
-            </Center>
-            <VStack flex={1} space={4} mb={6} px={10}>
-              <Input
-                placeholder="E-mail"
-                keyboardType="email-address"
-                autoCapitalize="none"
-              />
-              <Input placeholder="Senha" secureTextEntry />
-            </VStack>
+            <Input placeholder="Senha" secureTextEntry />
           </VStack>
         </ScrollView>
       </TouchableWithoutFeedback>
