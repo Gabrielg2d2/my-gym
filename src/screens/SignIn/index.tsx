6078f6de19bece4ea46258d1226c379734fdfe9d
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-24 19:38:18 -0300

    feat: add Heading in sign in

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import BackGroundImg from "@assets/background.png";
 import LogoSVG from "@assets/logo.svg";
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
 export function SignIn() {
   return (
@@ -18,6 +18,12 @@ export function SignIn() {
         <Text fontSize="sm" fontWeight="bold" color="gray.100">
           Treine sua mente e seu corpo
         </Text>
+      </Center>
+
+      <Center my={24}>
+        <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
