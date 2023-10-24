755cf3cb1993800668a14fa1f3a5e331730060fd
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-24 18:39:37 -0300

    refactor: home page

--- App.tsx
+++ App.tsx
@@ -2,10 +2,21 @@ import Home from "@screens/Home";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <View style={{ flex: 1 }}>
-      <Home />
+      {!fontsLoaded ? <View /> : <Home />}
       <StatusBar style="auto" />
     </View>
   );
