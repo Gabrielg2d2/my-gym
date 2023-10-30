0a53465e05ac48db4eacc4e0cf08d26ee8df81cd
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-30 09:35:37 -0300

    feat: 🎸 ProfileTemplate

--- src/screens/Profile/template/index.tsx
+++ src/screens/Profile/template/index.tsx
@@ -1,5 +1,6 @@
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "native-base";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack } from "native-base";
 
 export type IProfileTemplateProps = {};
 
@@ -7,6 +8,17 @@ export function ProfileTemplate(props: IProfileTemplateProps) {
   return (
     <VStack flex={1} bg="bgDefault">
       <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{
+              uri: "https://github.com/Gabrielg2d2.png",
+            }}
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
