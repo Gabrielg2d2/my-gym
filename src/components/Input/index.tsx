f131612d4503ef85f7d35d858696f918f179af5c
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2024-03-18 18:41:39 -0300

    refactor: 💡 component input
    ✅ Closes: c

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,24 +1,37 @@
-import { IInputProps, Input as NativeBaseInput } from "native-base";
+import { IInputProps, Input as NativeBaseInput, Text } from "native-base";
 
-type InputProps = IInputProps;
+type InputProps = IInputProps & {
+  error?: boolean;
+  textHelper?: string;
+};
 
-export function Input({ ...rest }: InputProps) {
+export function Input({ error = false, textHelper, ...rest }: InputProps) {
+  const colorBorder = error ? "red.500" : "green.500";
+
   return (
-    <NativeBaseInput
-      bg="gray.700"
-      h={14}
-      px={4}
-      borderWidth={0}
-      fontSize="md"
-      color="white"
-      fontFamily="body"
-      placeholderTextColor="gray.300"
-      _focus={{
-        bg: "gray.700",
-        borderWidth: 1,
-        borderColor: "green.500",
-      }}
-      {...rest}
-    />
+    <>
+      <NativeBaseInput
+        bg="gray.700"
+        h={14}
+        px={4}
+        fontSize="md"
+        color="white"
+        fontFamily="body"
+        placeholderTextColor="gray.300"
+        borderWidth={1}
+        borderColor={error ? "red.500" : "gray.700"}
+        _focus={{
+          bg: "gray.700",
+          borderWidth: 1,
+          borderColor: colorBorder,
+        }}
+        {...rest}
+      />
+      {textHelper && (
+        <Text fontSize="sm" color={error ? "red.500" : "gray.300"} mt={1}>
+          {textHelper}
+        </Text>
+      )}
+    </>
   );
 }
